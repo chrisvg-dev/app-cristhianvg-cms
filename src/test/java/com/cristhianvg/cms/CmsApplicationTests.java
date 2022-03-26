923b826af668b2b349a43f462a06293cfb538885
CRISTHIAN VILLEGAS GARCIA <cristianvg9692@gmail.com>
2022-03-26 15:42:12 -0600

    PLANTILLA CON DEPENDENCIAS BASE DEL PROYECTO

---
+++ src/test/java/com/cristhianvg/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristhianvg.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
